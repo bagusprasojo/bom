a9ca63f2e930b9d7ee20e2f413c35e3a8c0f663e
Bagus Laptop Baru <bagus@laptopbaru.com>
2026-09-17 15:27:49 +0700

    mysql

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
-﻿# config/urls.py
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('bagusprasojo/', admin.site.urls),
     path('', include('hpp.urls')),
 ]
